dcb4a76b0907bd03d6b465d0406084e764a8526f
sharma-neil <ns86.sharma@gmail.com>
2022-07-13 22:45:19 -0400

    upload

--- 4_Supply_Chain_Network_Design/Facility_Allocation_Python/facility_allocation_python.py
+++ 4_Supply_Chain_Network_Design/Facility_Allocation_Python/facility_allocation_python.py
@@ -1,42 +1,45 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Fri Sep 18 11:43:18 2020
-
-@author: haythamomar
+@author: Neil Sharma
 """
 import pandas as pd
-from pulp import *
+import pulp as pl
 
-dist= pd.read_excel('warehouse_city.xlsx')
-
+dist= pd.read_excel('/home/neil/repos/Supply-Chain-Design/4_Supply_Chain_Network_Design/Facility_Allocation_Python/warehouse_city.xlsx')
 dist= dist.set_index('Warehouse')
+print(dist)
 
-
+#demand for every customer
 demand=[10000,20000,33000,9000,60000,2500,35000]
+
+#setting row headers as potential warehouse location and columns as every customer
 warehouse= dist.index
 customers= dist.columns
 
+#maps every warehouse location to every customer location
 keys= [(w,c) for w in warehouse for c in customers]
 
+# attaches distance for every warehouse location to customer location
 dist_dict= { (w,c): dist.loc[w,c] for w in warehouse for c in customers}
 
-
-flows= LpVariable.dicts('flows',keys,cat='Binary')
-open_w= LpVariable.dicts('open_w',warehouse,cat='Binary')
+#creates a decision variable for whether the route from a warehouse to customer location has been opened
+flows= pl.LpVariable.dicts('flows',keys,cat='Binary')
+#creates a decision variable whether to open a warehouse or not
+open_w= pl.LpVariable.dicts('open_w',warehouse,cat='Binary')
 
 demand_dict= dict(zip(customers,demand))
 
-model= LpProblem('w_alloc',LpMinimize)
+#creating minimization objective function
+model= pl.LpProblem('w_alloc',pl.LpMinimize)
 
-model+= lpSum([demand_dict[(c)]* flows[(w,c)]* dist_dict[(w,c)] for c in customers for w in warehouse])
+#sum product the demand dictionary with the flows (warehouse to customer) and the distances
+model+= pl.lpSum([demand_dict[(c)]* flows[(w,c)]* dist_dict[(w,c)] for c in customers for w in warehouse])
 
 
 for c in customers:
-    model+= lpSum([flows[(w,c)] for w in warehouse])== 1
+    model+= pl.lpSum([flows[(w,c)] for w in warehouse])== 1
     
 
-model+= lpSum([open_w[(w)] for w in warehouse])==3
+model+= pl.lpSum([open_w[(w)] for w in warehouse])==3
 
 
 
@@ -58,7 +61,7 @@ flows_names= ['{} to {}'.format(w, c) for w in warehouse for c in customers]
 flows_quantity= [flows[(w,c)].varValue for w in warehouse for c in customers]
 flows_dict= dict(zip(flows_names,flows_quantity))
 
-average_distance= value(model.objective)/sum(demand)
+average_distance= pl.value(model.objective)/sum(demand)
 n_warehouses_opened= sum([open_w[(w)].varValue for w in warehouse])
 
 #### a simulation for distance Vs No of warhouses
@@ -66,16 +69,16 @@ n_warehouses_opened= sum([open_w[(w)].varValue for w in warehouse])
 def pulp_model(n):
 
    
-   model1= LpProblem('w_alloc',LpMinimize)
+   model1= pl.LpProblem('w_alloc',pl.LpMinimize)
 
-   model1+= lpSum([demand_dict[(c)]* flows[(w,c)]* dist_dict[(w,c)] for c in customers for w in warehouse])
+   model1+= pl.lpSum([demand_dict[(c)]* flows[(w,c)]* dist_dict[(w,c)] for c in customers for w in warehouse])
 
 
    for c in customers:
-       model1+= lpSum([flows[(w,c)] for w in warehouse])== 1
+       model1+= pl.lpSum([flows[(w,c)] for w in warehouse])== 1
     
 
-   model1+= lpSum([open_w[(w)] for w in warehouse])== n
+   model1+= pl.lpSum([open_w[(w)] for w in warehouse])== n
 
 
    for w in warehouse:
@@ -88,7 +91,7 @@ def pulp_model(n):
       model1+= open_w[(w)]>= flows[(w,'city 7')]
 
    model1.solve()
-   average_distance= value(model1.objective)/sum(demand)
+   average_distance= pl.value(model1.objective)/sum(demand)
    n_warehouses_opened= sum([open_w[(w)].varValue for w in warehouse])
    avg_warehouse_distance= {n_warehouses_opened: average_distance}
    return avg_warehouse_distance
